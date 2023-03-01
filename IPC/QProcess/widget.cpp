73c522acb2a6d1facb545967898691d81fbaa21e
fatma zayrek <fzayrek@gmail.com>
2023-03-01 10:10:14 +0300

    QProcess

---
+++ IPC/QProcess/widget.cpp
@@ -0,0 +1,78 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+#include <QFileDialog>
+#include <QDebug>
+#include <QMessageBox>
+#include <QPixmap>
+#include <QApplication>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    myProcess = new QProcess(this);
+
+    connect(myProcess, &QProcess::started, this, &Widget::started);
+    connect(myProcess, &QProcess::finished, this, &Widget::finished);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_choose_clicked()
+{
+    fileName = QFileDialog::getOpenFileName(this, tr("Select a File"), "C:/", tr("Programs (*.exe)"));
+
+    if(!fileName.isNull())
+    {
+        ui->lineEdit->setText(fileName);
+    }
+}
+
+
+void Widget::on_start_clicked()
+{
+    if(!fileName.isNull())
+    {
+        myProcess->start(fileName);
+    }
+
+    else
+    {
+        QPixmap myPixmap("://myImages/attach file.png");
+
+        QMessageBox msg;
+        msg.setInformativeText("You must select one file");
+        msg.setIconPixmap(myPixmap);
+        msg.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+        int result = msg.exec();
+
+        if(result == QMessageBox::Cancel)
+        {
+            QApplication::closeAllWindows();
+        }
+    }
+}
+
+void Widget::on_stop_clicked()
+{
+    myProcess->close();
+}
+
+void Widget::finished(int exitCode)
+{
+    qDebug() << "exitCode: " << exitCode;
+//    qDebug() << "exitStatus: " << exitStatus;
+}
+
+void Widget::started()
+{
+    qDebug() << "Process started..." ;
+}
+
---
+++ IPC/QProcess/widget.h
@@ -0,0 +1,33 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QProcess>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_choose_clicked();
+    void on_start_clicked();
+    void on_stop_clicked();
+
+    void finished(int exitCode);
+    void started();
+
+private:
+    Ui::Widget *ui;
+
+    QString fileName;
+    QProcess *myProcess;
+};
+#endif // WIDGET_H
